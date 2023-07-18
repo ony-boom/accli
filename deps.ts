8e913cb090b0d457499b320f7770b4cfb05432a3
Ony <orakotonirina@bocasay.com>
2023-07-18 07:23:13 +0300

    MVP with cliffy

--- deps.ts
+++ deps.ts
@@ -1,6 +1,5 @@
 import axiod from "https://deno.land/x/axiod@0.26.2/mod.ts";
 import { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
-import { parse } from "https://deno.land/std@0.184.0/flags/mod.ts";
 import { join } from "https://deno.land/std@0.190.0/path/mod.ts";
 import os from "https://deno.land/x/dos@v0.11.0/mod.ts";
 import {
@@ -8,5 +7,23 @@ import {
   walk,
   move,
 } from "https://deno.land/std@0.190.0/fs/mod.ts";
+import gradient from "npm:gradient-string@2.0.2";
+import * as cliffy from "https://deno.land/x/cliffy@v1.0.0-rc.2/prompt/mod.ts";
+import * as colors from "https://deno.land/x/cliffy@v1.0.0-rc.2/ansi/mod.ts";
+import { wait, Spinner } from "https://deno.land/x/wait@0.1.13/mod.ts";
 
-export { axiod, z, parse, os, join, existsSync, walk, move };
+
+export {
+  axiod,
+  z,
+  os,
+  join,
+  existsSync,
+  walk,
+  move,
+  gradient,
+  colors,
+  cliffy,
+  wait,
+  Spinner
+};
